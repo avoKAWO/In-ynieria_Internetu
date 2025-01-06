683786a862abda33d9496976a86e7a0b60269d44
avoKAWO <wodakarol200@gmail.com>
2025-01-06 13:51:09 +0100

    Lab 2 - zad 1

---
+++ lab2/src/index.ts
@@ -0,0 +1,12 @@
+import {DataProcessor} from "./data-processor";
+
+const proc = new DataProcessor<string>();
+proc.addData("X");
+proc.addData("13");
+
+proc.processData((d)=>{
+    console.log(d);
+});
+
+const filtered = proc.filterData((d)=> d.length >= 2);
+console.table(filtered);
